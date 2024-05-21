868fd38a3f607842cc99a8164b2fdde202c21a42
saif <saif@saifs-MacBook-Pro.local>
2024-05-20 22:39:19 -0400

    first commit

---
+++ dist/assets/Empty-369f49b9.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,b as o,d as t}from"./index-04aa5200.js";const c={},a={className:"card"},n=t("h5",null,"Empty Page",-1),r=t("p",null,"Use this page to start from scratch and place your custom content.",-1),_=[n,r];function d(l,i){return s(),o("div",a,_)}const p=e(c,[["render",d]]);export{p as default};
---
+++ dist/assets/PaymentDemo-8369d650.js
@@ -0,0 +1,1 @@
+import{_ as t,o,b as s,d as e}from"./index-04aa5200.js";const n={},c={class:"flex align-items-center py-5 px-3"},a=e("i",{class:"pi pi-fw pi-money-bill mr-2 text-2xl"},null,-1),i=e("p",{class:"m-0 text-lg"},"Payment Component Content via Child Route",-1),_=[a,i];function l(r,m){return o(),s("div",c,_)}const p=t(n,[["render",l]]);export{p as default};
