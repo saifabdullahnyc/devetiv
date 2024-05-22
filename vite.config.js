b791c37352ab853970a184670fa5ab4114bdfebd
saif <saif@saifs-MacBook-Pro.local>
2024-05-21 20:55:44 -0400

    ..

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(() => {
     return {
         plugins: [vue()],
-        // resolve: {
-        //     alias: {
-        //         '@': fileURLToPath(new URL('./src', import.meta.url))
-        //     }
-        // }
-        base: '/devetiv'
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        //base: '/devetiv/'
     };
 });
